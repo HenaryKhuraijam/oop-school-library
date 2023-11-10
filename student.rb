5ff748f84960ed9fe55851811afd5bd43bc9e754
HenaryKhuraijam <henarykhu123@gmail.com>
2023-11-10 23:54:27 +0530

    Update person.rd,student.rd and teacher.rd

--- student.rb
+++ student.rb
@@ -1,19 +1,19 @@
 require './person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission:)
     @classroom = classroom
   end
 
-  def classroom=(classroom)
+  def classrooms=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
-    '¯(ツ)/¯'
+    "¯\(ツ)/¯"
   end
 end
